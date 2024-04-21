7b354d88a13c1c90bc9825e5288b05252b12902e
RougherO <chiragchowdhury51@gmail.com>
2024-04-21 13:59:16 +0530

    Changed TCP Server Client to polling model instead of thread

--- client_tcp.cpp
+++ client_tcp.cpp
@@ -7,45 +7,26 @@
 
 #include <arpa/inet.h>
 #include <sys/socket.h>
-#include <sys/types.h>
 #include <poll.h>
 #include <fcntl.h>
 #include <unistd.h>
 
-static const int32_t stdin_fd  = fileno(stdin);
-static const int32_t stdout_fd = fileno(stdout);
-static const int32_t stderr_fd = fileno(stderr);
-
-template <int32_t Domain, int32_t Type, int32_t Proto = 0>
+template <int32_t Domain>
 class Client {
 public:
-    int32_t const domain   = Domain;
-    int32_t const type     = Type;
-    int32_t const protocol = Proto;
+    int32_t const domain = Domain;
 
     Client(std::string const& uname) noexcept
         : m_uname { std::move(uname) }
-        , m_socket { socket(Domain, Type, Proto) }
+        , m_socket { socket(Domain, SOCK_STREAM, 0) }
     {
         if (m_socket == -1) {
             std::fprintf(stderr, "Could not create socket for client : %s\n", m_uname.c_str());
             std::fflush(stderr);
-            // Hanlde error and return
             return;
         }
-        int32_t flags = fcntl(m_socket, F_GETFL);
-        if (flags == -1) {
-            std::fprintf(stderr, "Could not get socket flags for client : %s\n", m_uname.c_str());
-            std::fflush(stderr);
-            // Handle error and return
-            return;
-        }
-        if (fcntl(m_socket, F_SETFL, flags | O_NONBLOCK) == -1) {
-            std::fprintf(stderr, "Could not set socket flags for client : %s\n", m_uname.c_str());
-            std::fflush(stderr);
-            // Handle error and return
-            return;
-        }
+        m_pollfd_set[FD_INDEX::STDIN]       = { STDIN_FILENO, POLLIN };
+        m_pollfd_set[FD_INDEX::SERVER_SOCK] = { m_socket, POLLIN };
     }
 
     Client(Client const&)             = delete;
@@ -56,47 +37,47 @@ public:
     template <typename SockAddrType>
     void connect_to(SockAddrType&& server_address) noexcept
     {
-        while (1) {
-            if (connect(m_socket, reinterpret_cast<sockaddr*>(&server_address), sizeof(SockAddrType)) == -1) {
-                if (errno == EINPROGRESS) {
-                    std::fputs("Connecting...\n", stdout);
-                    std::fflush(stdout);
-                    continue;
-                }
-                if (errno == EISCONN) {
-                    int32_t send_uname = send(m_socket, m_uname.c_str(), m_uname.size(), 0);
-                    if (send_uname == -1) {
-                        std::fputs("Could not send username\n", stderr);
-                    }
-                    m_is_conn = true;
-                    std::fputs("Connection established\n", stdout);
-                    std::fflush(stdout);
-                    return;
-                }
-                std::fprintf(stderr,
-                             "Could not connect to server IP : %s PORT : %hu\n",
-                             inet_ntop(AF_INET, &server_address.sin_addr, m_read_buffer.data(), INET_ADDRSTRLEN),
-                             ntohs(server_address.sin_port));
+        if (connect(m_socket, reinterpret_cast<sockaddr*>(&server_address), sizeof(SockAddrType)) != -1) {
+            int32_t send_uname = send(m_socket, m_uname.c_str(), m_uname.size(), 0);
+            if (send_uname == -1) {
+                std::fputs("Could not send username\n", stderr);
+                std::fflush(stderr);
+            }
+            std::fputs("Connection established\n", stdout);
+            std::fflush(stdout);
+            return;
+        }
+        std::fprintf(stderr,
+                     "Could not connect to server IP : %s PORT : %hu\n",
+                     inet_ntop(AF_INET, &server_address.sin_addr, m_read_buffer.data(), INET_ADDRSTRLEN),
+                     ntohs(server_address.sin_port));
+        std::fflush(stderr);
+        return;
+    }
+
+    void communicate(int32_t tout_in_mill = 1000)
+    {
+        for (int32_t poll_stat; !m_close_conn;) {
+            poll_stat = poll(m_pollfd_set.data(), m_pollfd_set.size(), tout_in_mill);
+            if (poll_stat == -1) {
+                std::fprintf(stderr, "Polling failed\n");
                 std::fflush(stderr);
                 return;
+            }
+            if (poll_stat > 0) {
+                if (m_pollfd_set[STDIN_FILENO].revents & POLLIN) {
+                    m_send_msg();
+                    continue;
+                }
+                if (m_pollfd_set[FD_INDEX::SERVER_SOCK].revents & POLLIN) {
+                    m_receive_msg();
+                }
             }
         }
     }
 
-    void communicate()
-    {
-        if (!m_is_conn) {
-            return;
-        }
-        m_read_thread = std::thread(&Client::m_receive_msg, this);
-        m_send_msg();
-    }
-
     ~Client() noexcept
     {
-        if (m_read_thread.joinable()) {
-            m_read_thread.join();
-        }
         if (m_socket != -1) {
             close(m_socket);
         }
@@ -105,60 +86,56 @@ public:
 private:
     void m_receive_msg() noexcept
     {
-        for (int32_t read_bytes {}; !m_close_conn;) {
-            read_bytes = recv(m_socket, m_read_buffer.data(), s_max_buffer_size, 0);
-            if (read_bytes == -1) {
-                if (errno == EWOULDBLOCK || errno == EAGAIN) {
-                    continue;
-                }
-                std::fputs("Could not receive complete message\n", stderr);
-                std::fflush(stderr);
-            } else if (read_bytes == 0) {
-                std::fputs("Server closed connection\n", stdout);
-                std::fflush(stdout);
-                m_close_conn = true;
-                continue;
-            }
-            std::fputs("Message from [SERVER]: ", stdout);
-            std::fwrite(m_read_buffer.data(), sizeof(char), read_bytes, stdout);
+        int32_t read_bytes = recv(m_socket, m_read_buffer.data(), s_MAX_BUFFER_SIZE, 0);
+        if (read_bytes == -1) {
+            std::fputs("Could not receive complete message\n", stderr);
+            std::fflush(stderr);
+        } else if (read_bytes == 0) {
+            std::fputs("Server closed connection\n", stdout);
             std::fflush(stdout);
+            m_close_conn = true;
+            return;
         }
+        std::fwrite(m_read_buffer.data(), sizeof(char), read_bytes, stdout);
+        std::fflush(stdout);
     }
 
     void m_send_msg() noexcept
     {
-        for (int32_t send_bytes {}; !m_close_conn;) {
-            send_bytes = read(stdin_fd, m_write_buffer.data(), s_max_buffer_size);
+        int32_t send_bytes         = read(STDIN_FILENO, m_write_buffer.data(), s_MAX_BUFFER_SIZE);
+        m_write_buffer[send_bytes] = 0;
 
-            if (send_bytes == -1) {
-                if (errno == EWOULDBLOCK) {
-                    continue;
-                }
-                std::fputs("Could not send message\n", stderr);
-                std::fflush(stderr);
-            }
-
-            if (m_write_buffer[0] == '0') {
-                std::fputs("Closing connection\n", stdout);
-                std::fflush(stdout);
-                m_close_conn = true;
-                break;
-            }
-            send_bytes = send(m_socket, m_write_buffer.data(), send_bytes, 0);
+        if (send_bytes == -1) {
+            std::fputs("Could not send message\n", stderr);
+            std::fflush(stderr);
+            return;
         }
+
+        if (m_write_buffer[0] == '0') {
+            std::fputs("Closing connection\n", stdout);
+            std::fflush(stdout);
+            m_close_conn = true;
+            return;
+        }
+
+        send(m_socket, m_write_buffer.data(), send_bytes, 0);
     }
 
 public:
-    static constexpr uint16_t s_max_buffer_size { 1024 };
+    static constexpr uint16_t s_MAX_BUFFER_SIZE { 1024 };
+    static constexpr uint16_t s_EXTRA_BUFFER_SIZE { 256 };
 
 private:
+    enum FD_INDEX {
+        STDIN,
+        SERVER_SOCK
+    };
     std::string const m_uname;
     int32_t const m_socket;
-    bool m_is_conn {};
-    std::atomic<bool> m_close_conn {};
-    std::thread m_read_thread;
-    std::array<char, s_max_buffer_size> m_read_buffer {};
-    std::array<char, s_max_buffer_size> m_write_buffer {};
+    bool m_close_conn {};
+    std::array<pollfd, 2> m_pollfd_set;
+    std::array<char, s_MAX_BUFFER_SIZE + 1 + s_EXTRA_BUFFER_SIZE> m_read_buffer {};
+    std::array<char, s_MAX_BUFFER_SIZE + 1 + s_EXTRA_BUFFER_SIZE> m_write_buffer {};
 };
 
 int32_t main(int32_t argc, char** argv)
@@ -170,26 +147,12 @@ int32_t main(int32_t argc, char** argv)
         std::exit(64);
     }
 
-    int32_t flag = fcntl(stdin_fd, F_GETFL);
-    if (flag == -1) {
-        std::fputs("Could not get stdin flags\n", stderr);
-        std::fflush(stderr);
-        std::exit(1);
-    }
-
-    if (fcntl(stdin_fd, F_SETFL, flag | O_NONBLOCK) == -1) {
-        std::fputs("Could not set stdin flags\n", stderr);
-        std::fflush(stderr);
-        std::exit(1);
-    }
-
-    // Server configuration
     sockaddr_in server_address;
     server_address.sin_family = AF_INET;
-    inet_pton(AF_INET, argv[1], &server_address.sin_addr);   // server_address.sin_addr.s_addr = inet_addr(argv[1]);
+    inet_pton(AF_INET, argv[1], &server_address.sin_addr);
     server_address.sin_port = htons(std::stoul(argv[2]));
 
-    Client<AF_INET, SOCK_STREAM> c { argv[3] };
+    Client<AF_INET> c { argv[3] };
     c.connect_to(std::move(server_address));
     c.communicate();
 }
--- server_tcp.cpp
+++ server_tcp.cpp
@@ -1,239 +1,199 @@
-#include <atomic>
-#include <cerrno>
+#include <algorithm>
 #include <cstdio>
 #include <cstring>
 #include <string>
 #include <array>
-#include <thread>
-#include <unordered_map>
+#include <map>
 
 #include <arpa/inet.h>
 #include <sys/socket.h>
-#include <fcntl.h>
 #include <poll.h>
 #include <unistd.h>
 
-static const int32_t stdin_fd  = fileno(stdin);
-static const int32_t stdout_fd = fileno(stdout);
-static const int32_t stderr_fd = fileno(stderr);
-
-template <int32_t Domain, int32_t Type, int32_t Proto = 0>
-class Server {
+template <int32_t Domain, int32_t Protocol = 0>
+class TCPServer {
 public:
-    int32_t const domain   = Domain;
-    int32_t const type     = Type;
-    int32_t const protocol = Proto;
+    int32_t const domain = Domain;
 
-    Server(char const* ip_addr, uint16_t port_num, uint16_t listeners) noexcept
-        : m_accept_socket { socket(Domain, Type, Proto) }
+    TCPServer(char const* ip_addr, uint16_t port_num, uint16_t listeners) noexcept
+        : m_ACC_SOCK { socket(Domain, SOCK_STREAM, Protocol) }
     {
-        if (m_accept_socket == -1) {
-            std::fprintf(stderr, "Could not create socket for server\n");
+        if (m_ACC_SOCK == -1) {
+            std::fprintf(stderr, "Could not create socket for TCPServer\n");
             std::fflush(stderr);
-            // Hanlde error and return
             return;
         }
 
-        int32_t flags = fcntl(m_accept_socket, F_GETFL);
-        if (flags == -1) {
-            std::fprintf(stderr, "Could not get socket flags for server\n");
+        m_TCPServer_address.sin_family = Domain;
+        inet_pton(Domain, ip_addr, &m_TCPServer_address.sin_addr);
+        m_TCPServer_address.sin_port = htons(port_num);
+
+        if (bind(m_ACC_SOCK, reinterpret_cast<sockaddr*>(&m_TCPServer_address), sizeof(m_TCPServer_address)) == -1) {
+            std::fprintf(stderr, "Could not bind TCPServer to the given address\n");
             std::fflush(stderr);
-            // Handle error and return
-            return;
-        }
-        if (fcntl(m_accept_socket, F_SETFL, flags | O_NONBLOCK) == -1) {
-            std::fprintf(stderr, "Could not set socket flags for server\n");
-            std::fflush(stderr);
-            // Handle error and return
             return;
         }
 
-        m_server_address.sin_family = Domain;
-        inet_pton(Domain, ip_addr, &m_server_address.sin_addr);
-        m_server_address.sin_port = htons(port_num);
-
-        if (bind(m_accept_socket, reinterpret_cast<sockaddr*>(&m_server_address), sizeof(m_server_address)) == -1) {
-            std::fprintf(stderr, "Could not bind server to the given address\n");
-            std::fflush(stderr);
-            // Handle error and return
-            return;
-        }
-
-        if (listen(m_accept_socket, listeners) == -1) {
+        if (listen(m_ACC_SOCK, listeners) == -1) {
             std::fprintf(stderr, "Could not listen on given PORT and IP\n");
             std::fflush(stderr);
-            // Handle error and return
             return;
         }
 
-        std::fprintf(stdout, "Server listening on %s:%d\n", ip_addr, port_num);
+        m_pollfd_set[FD_INDEX::STDIN]  = { STDIN_FILENO, POLLIN };
+        m_pollfd_set[FD_INDEX::ACCEPT] = { m_ACC_SOCK, POLLIN };
+
+        std::fprintf(stdout, "TCPServer listening on %s:%d\n", ip_addr, port_num);
     }
 
-    Server(Server const&)             = delete;
-    Server& operator=(Server const&)  = delete;
-    Server(Server const&&)            = delete;
-    Server& operator=(Server const&&) = delete;
-
-    void accept_conn() noexcept
-    {
-        for (int32_t client_sock; m_conns < s_max_conns;) {
-            client_sock = accept(m_accept_socket, nullptr, nullptr);
-            if (client_sock == -1) {
-                if (errno == EWOULDBLOCK || errno == EAGAIN) {
-                    break;
-                } else {
-                    std::fprintf(stderr, "Could not accept connection\n");
-                    std::fflush(stderr);
-                    return;
-                }
-            }
-            for (int32_t uname_size;;) {
-                uname_size = recv(client_sock, m_read_buffer.data(), s_max_buffer_size, 0);
-                if (uname_size == -1) {
-                    if (errno != EWOULDBLOCK && errno != EAGAIN) {
-                        std::fprintf(stderr, "Could not receive username");
-                        std::fflush(stderr);
-                    }
-                    continue;
-                }
-                std::fprintf(stdout, "[%.*s] connected\n", uname_size, m_read_buffer.data());
-                m_client_uname_sock_map[client_sock].assign(m_read_buffer.data(), uname_size);
-                m_client_pollfds[m_conns].fd     = client_sock;
-                m_client_pollfds[m_conns].events = POLLIN;
-                m_conns++;
-                break;
-            }
-        }
-    }
+    TCPServer(TCPServer const&)             = delete;
+    TCPServer& operator=(TCPServer const&)  = delete;
+    TCPServer(TCPServer const&&)            = delete;
+    TCPServer& operator=(TCPServer const&&) = delete;
 
     void start(int32_t tout_in_mill = 1000) noexcept
     {
-        m_read_thread                   = std::thread(&Server::m_check_conns, this, tout_in_mill);
-        acc_and_stdin_pollfds[0].fd     = m_accept_socket;
-        acc_and_stdin_pollfds[0].events = POLLIN;
-        acc_and_stdin_pollfds[1].fd     = stdin_fd;
-        acc_and_stdin_pollfds[1].events = POLLIN;
-
         for (int32_t poll_stat; !m_close_conn;) {
-            poll_stat = poll(acc_and_stdin_pollfds.data(), acc_and_stdin_pollfds.size(), tout_in_mill);
+            poll_stat = poll(m_pollfd_set.data(), 2 + m_conns, tout_in_mill);
             if (poll_stat == -1) {
                 std::fprintf(stderr, "Polling failed\n");
                 std::fflush(stderr);
-                // Handle error and return;
                 return;
-            } else if (poll_stat > 0) {
-                // Check if server socket has any new connection)
-                if (acc_and_stdin_pollfds[0].revents & POLLIN) {
-                    accept_conn();
-                    continue;   // Repolling to get new events for all sockets
+            }
+            if (poll_stat > 0) {
+                if (m_pollfd_set[FD_INDEX::ACCEPT].revents & POLLIN) {
+                    m_accept_conn() ? m_broadcast(FD_INDEX::CLIENTS + m_conns - 1) : void();
+                    continue;
                 }
-                // Check if server has any message to broadcast
-                if (acc_and_stdin_pollfds[1].revents & POLLIN) {
-                    int32_t msg_len = read(stdin_fd, m_write_buffer.data(), s_max_buffer_size);
+                if (m_pollfd_set[FD_INDEX::STDIN].revents & POLLIN) {
+                    int32_t msg_len         = read(m_pollfd_set[FD_INDEX::STDIN].fd, m_write_buffer.data(), s_MAX_BUFFER_SIZE);
+                    m_write_buffer[msg_len] = 0;
                     if (m_write_buffer[0] == '0') {
-                        std::fputs("Shutting Down Server\n", stdout);
+                        std::fputs("Shutting Down TCPServer\n", stdout);
                         std::fflush(stdout);
                         m_close_conn = true;
                         break;
                     }
-                    for (uint8_t client_index {}; client_index < m_conns; ++client_index) {
-                        m_write_client(client_index, msg_len);
+                    m_broadcast(0);
+                    continue;
+                }
+                for (auto* client_ptr = m_pollfd_set.begin() + FD_INDEX::CLIENTS; client_ptr < m_pollfd_set.begin() + FD_INDEX::CLIENTS + m_conns; ++client_ptr) {
+                    if (client_ptr->revents & POLLIN) {
+                        m_read_client(client_ptr - m_pollfd_set.begin()) ? m_broadcast(client_ptr - m_pollfd_set.begin()) : void();
                     }
                 }
             }
         }
     }
 
-    ~Server() noexcept
+    ~TCPServer() noexcept
     {
-        if (m_read_thread.joinable()) {
-            m_read_thread.join();
-        }
-        if (m_accept_socket != -1) {
-            close(m_accept_socket);
-        }
+        std::for_each_n(m_pollfd_set.begin(), m_conns + FD_INDEX::CLIENTS, [](pollfd const& poll_fd) {
+            close(poll_fd.fd);
+        });
     }
 
 private:
-    void m_check_conns(int32_t tout_in_mill) noexcept
+    bool m_accept_conn() noexcept
     {
-        for (int32_t poll_stat; !m_close_conn;) {
-            poll_stat = poll(m_client_pollfds.data(), m_conns, tout_in_mill);
-            if (poll_stat == -1) {
-                std::fprintf(stderr, "Polling failed\n");
+        if (m_conns < s_MAX_CONNS) {
+            int32_t client_sock = accept(m_ACC_SOCK, nullptr, nullptr);
+            if (client_sock == -1) {
+                std::fprintf(stderr, "Could not accept connection\n");
                 std::fflush(stderr);
-                // Handle error and return;
-                return;
-            } else if (poll_stat > 0) {
-                for (int32_t client_index = 0; client_index < m_conns; client_index++) {
-                    if (m_client_pollfds[client_index].revents == 0) {   // Skip clients which don't have any events
-                        continue;
-                    }
-                    if (m_client_pollfds[client_index].revents & POLLIN) {
-                        m_read_client(client_index);
-                    }
-                }
+                return false;
             }
+            int32_t uname_size = recv(client_sock, m_read_buffer.data(), s_MAX_BUFFER_SIZE, 0);
+            if (uname_size == -1) {
+                std::fprintf(stderr, "Could not receive username");
+                std::fflush(stderr);
+            }
+            int32_t msg_len         = std::sprintf(m_write_buffer.data(), "[%.*s] connected\n", uname_size, m_read_buffer.data());
+            m_write_buffer[msg_len] = 0;
+
+            m_client_uname_sock_map[client_sock].assign(m_read_buffer.data(), uname_size);
+            m_pollfd_set[FD_INDEX::CLIENTS + m_conns].fd     = client_sock;
+            m_pollfd_set[FD_INDEX::CLIENTS + m_conns].events = POLLIN;
+            m_conns++;
+
+            std::fputs(m_write_buffer.data(), stdout);
+            std::fflush(stdout);
+            return true;
         }
-        for (uint8_t client_index {}; client_index < m_conns; ++m_conns) {
-            close(m_client_pollfds[client_index].fd);
-        }
+        std::fprintf(stdout, "Maximum number of connections reached\n");
+        return false;
     }
 
-    void m_read_client(int32_t client_index) noexcept
+    bool m_read_client(int32_t client_index) noexcept
     {
-        int32_t read_bytes = recv(m_client_pollfds[client_index].fd, m_read_buffer.data(), m_read_buffer.size(), 0);
+        int32_t read_bytes = recv(m_pollfd_set[client_index].fd, m_read_buffer.data(), m_read_buffer.size(), 0);
         if (read_bytes == -1) {
             std::fprintf(stderr, "Could not receive complete message\n");
             std::fflush(stderr);
-            // Handle error and return
-            return;
+            return false;
         }
         if (read_bytes == 0) {
-            std::fprintf(stdout, "%s disconnected\n", m_client_uname_sock_map[m_client_pollfds[client_index].fd].data());
-            std::fflush(stdout);
+            int32_t msg_len         = std::sprintf(m_write_buffer.data(), "[%s] disconnected\n", m_client_uname_sock_map[m_pollfd_set[client_index].fd].c_str());
+            m_write_buffer[msg_len] = 0;
+
             m_close_client(client_index);
-            return;
-            // Close connection and swap it with an active connection to
-            // compress the number of active connections
+
+            std::fputs(m_write_buffer.data(), stdout);
+            std::fflush(stdout);
+
+            return true;
         }
-        std::fprintf(stdout,
-                     "Message from [%s]: %.*s",
-                     m_client_uname_sock_map[m_client_pollfds[client_index].fd].data(), read_bytes, m_read_buffer.data());
-        std::fflush(stdout);
+        int32_t msg_len = std::sprintf(
+            m_write_buffer.data(),
+            "Message from [%s]: %.*s", m_client_uname_sock_map[m_pollfd_set[client_index].fd].c_str(), read_bytes, m_read_buffer.data());
+        m_write_buffer[msg_len] = 0;
+        return true;
     }
 
     inline void m_close_client(int32_t client_index) noexcept
     {
-        close(m_client_pollfds[client_index].fd);
-        std::swap(m_client_pollfds[client_index], m_client_pollfds[m_conns - 1]);
+        close(m_pollfd_set[client_index].fd);
+        m_pollfd_set[client_index].fd = m_pollfd_set[FD_INDEX::CLIENTS + m_conns - 1].fd;
         m_conns--;
     }
 
-    void m_write_client(int32_t client_index, uint16_t msg_len) noexcept
+    void m_broadcast(int32_t client_index) noexcept
     {
-        int32_t send_bytes = send(m_client_pollfds[client_index].fd, m_write_buffer.data(), msg_len, 0);
-        if (send_bytes == -1) {
-            std::fputs("Could not send message\n", stderr);
-            std::fflush(stderr);
-        }
+        size_t msg_len = std::strlen(m_write_buffer.data());
+        std::for_each_n(m_pollfd_set.begin() + FD_INDEX::CLIENTS, m_conns, [=, index = int { FD_INDEX::CLIENTS }](pollfd const& poll_fd) mutable {
+            if (index != client_index) {
+                int32_t send_bytes = send(m_pollfd_set[index].fd, m_write_buffer.data(), msg_len, 0);
+                if (send_bytes == -1) {
+                    std::printf("client index: %d, index: %d", client_index, index);
+                    std::fputs("Could not send message\n", stderr);
+                    std::fflush(stderr);
+                }
+            }
+            index += 1;
+        });
     }
 
 public:
-    static constexpr uint8_t s_max_conns { 50 };
-    static constexpr uint16_t s_max_buffer_size { 1024 };
+    static constexpr uint8_t s_MAX_CONNS { 50 };
+    static constexpr uint16_t s_MAX_BUFFER_SIZE { 1024 };
+    static constexpr uint16_t s_EXTRA_BUFFER_SIZE { 256 };
 
 private:
-    int32_t const m_accept_socket;
+    enum FD_INDEX {
+        STDIN,
+        ACCEPT,
+        CLIENTS
+    };
+
+    int32_t const m_ACC_SOCK;
+    bool m_close_conn {};
     uint8_t m_conns {};
-    sockaddr_in m_server_address;
-    std::atomic<bool> m_close_conn {};
-    std::array<char, s_max_buffer_size> m_read_buffer;
-    std::array<char, s_max_buffer_size> m_write_buffer;
-    std::array<pollfd, 2> acc_and_stdin_pollfds;
-    std::array<pollfd, s_max_conns> m_client_pollfds;
-    std::unordered_map<uint16_t, std::string> m_client_uname_sock_map;
-    std::thread m_read_thread;
+    sockaddr_in m_TCPServer_address;
+    std::array<char, s_MAX_BUFFER_SIZE + 1 + s_EXTRA_BUFFER_SIZE> m_read_buffer;
+    std::array<char, s_MAX_BUFFER_SIZE + 1 + s_EXTRA_BUFFER_SIZE> m_write_buffer;
+    std::array<pollfd, 2 + s_MAX_CONNS> m_pollfd_set;
+
+    std::map<uint16_t, std::string> m_client_uname_sock_map;
 };
 
 auto main(int32_t argc, char** argv) -> int32_t
@@ -243,7 +203,7 @@ auto main(int32_t argc, char** argv) -> int32_t
         std::exit(64);
     }
 
-    Server<AF_INET, SOCK_STREAM> server {
+    TCPServer<AF_INET> server {
         argv[1],
         static_cast<uint16_t>(std::stoul(argv[2])),
         static_cast<uint16_t>(std::stoul(argv[3]))
